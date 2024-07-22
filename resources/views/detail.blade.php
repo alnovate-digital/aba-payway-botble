f7bb4a4990f8e4b4540c7c9d96316de1f7a9e0e7
Vince Satsong <vince@satsong.net>
2024-07-22 15:54:19 +0000

    Remove unnecessaries

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,11 +1,4 @@
 @if ($payment)
-    <br>
-    <p>{{ trans('plugins/payment::payment.payment_id') }}: {{ $payment->id }}</p>
-    <p>{{ trans('plugins/payment::payment.amount') }}: {{ $payment->amount / 100 }} {{ $payment->currency }}</p>
-    <p>{{ trans('plugins/payment::payment.email') }}: {{ $payment->email }}</p>
-    <p>{{ trans('plugins/payment::payment.phone') }}: {{ $payment->contact }}</p>
-    <p>{{ trans('core/base::tables.created_at') }}: {{ BaseHelper::formatDate($payment->created_at) }}</p>
     <hr>
-
     @include('plugins/payment::partials.view-payment-source')
 @endif
