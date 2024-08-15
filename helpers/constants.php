c845176d7a7f0c06d0a78a26c80aafbfd9d40f9a
Vince Satsong <vince@satsong.net>
2024-08-15 16:12:10 +0000

    Remove unnecessaries

--- helpers/constants.php
+++ helpers/constants.php
@@ -3,23 +3,3 @@
 if (! defined('PAYWAY_PAYMENT_METHOD_NAME')) {
     define('PAYWAY_PAYMENT_METHOD_NAME', 'payway');
 }
-
-if (! defined('ABAPAY_PAYMENT_OPTION_NAME')) {
-    define('ABAPAY_PAYMENT_OPTION_NAME', 'abapay');
-}
-
-if (! defined('KHQR_PAYMENT_OPTION_NAME')) {
-    define('KHQR_PAYMENT_OPTION_NAME', 'bakong');
-}
-
-if (! defined('CARDS_PAYMENT_OPTION_NAME')) {
-    define('CARDS_PAYMENT_OPTION_NAME', 'cards');
-}
-
-if (! defined('WECHAT_PAYMENT_OPTION_NAME')) {
-    define('WECHAT_PAYMENT_OPTION_NAME', 'wechat');
-}
-
-if (! defined('ALIPAY_PAYMENT_OPTION_NAME')) {
-    define('ALIPAY_PAYMENT_OPTION_NAME', 'alipay');
-}
